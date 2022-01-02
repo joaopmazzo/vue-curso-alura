f8d58f88b823661a53068e9c2dbee5826a744c9c
João Paulo Mazzo <mazzojp@gmail.com>
2022-01-02 19:08:34 -0300

    Realiza uma 'configuração inicial' me nosso Vue.app

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,90 +1,13 @@
 <template>
-  <div class="corpo">
-    <h1 class="centralizado" v-text="titulo"></h1>
-    <input
-      type="search"
-      class="filtro"
-      @input="filtro = $event.target.value"
-      placeholder="Filtre por parte do título1"
-    />
-    {{ filtro }}
-    <ul class="lista_foto">
-      <li
-        class="lista_foto-itens"
-        v-for="nomeGenerico of fotosComFiltro"
-        :key="nomeGenerico"
-      >
-        <meu-painel :titulo="nomeGenerico.titulo">
-          <imagem-responsiva
-            :src="nomeGenerico.url"
-            :alt="nomeGenerico.titulo"
-          />
-        </meu-painel>
-      </li>
-    </ul>
-  </div>
+  <div class="corpo"></div>
 </template>
 
-<script>
-import Painel from "./components/shared/painel/Painel.vue";
-import ImagemResponsiva from "./components/shared/imagem-responsiva/ImagemResponsiva.vue";
-
-export default {
-  components: {
-    "meu-painel": Painel,
-    "imagem-responsiva": ImagemResponsiva
-  },
-  data() {
-    return {
-      titulo: "AluraPic",
-      fotos: [],
-      filtro: ""
-    };
-  },
-  computed: {
-    fotosComFiltro() {
-      if (this.filtro) {
-        /* filtrar */
-        let exp = new RegExp(this.filtro.trim(), "i");
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      } else {
-        return this.fotos;
-      }
-    }
-  },
-  created() {
-    this.$http
-      .get("http://localhost:3000/v1/fotos") // realiza a consulta no endereço da API
-      .then(res => res.json()) // pega os dados do retorno e transforma em um json (que tambem é uma promise)
-      .then(
-        fotos => (this.fotos = fotos),
-        err => console.log(err)
-      ); // pega esse json e passa para o nosso array de fotos e, caso não consiga retornar nada dessa "promessa", mostra o erro no console
-  }
-};
-</script>
+<script></script>
 
 <style>
 .corpo {
   font-family: Arial, Helvetica, sans-serif;
   width: 96%;
   margin: 0 auto;
-}
-
-.centralizado {
-  text-align: center;
-}
-
-.lista_foto {
-  list-style: none;
-}
-
-.lista_foto .lista_foto-itens {
-  display: inline-block;
-}
-
-.filtro {
-  display: block;
-  width: 100%;
 }
 </style>
